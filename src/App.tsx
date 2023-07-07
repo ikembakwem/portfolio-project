ceeac7ff3acaff80bb80c3615375f0988d57f4fa
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2023-07-07 11:04:15 +0100

    Added a Hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled";
 import { AnimatedParagraph } from "./components/textRevealAnimation";
 import { Header } from "./components/Header";
+import { Hero } from "./components/Hero";
 
 function App() {
   return (
@@ -8,21 +9,7 @@ function App() {
       <Header />
       <main>
         <Container>
-          <Content>
-            <Intro>
-              Hello ✋,
-              <br />
-            </Intro>
-            <Title>am Peter a Front-End Developer</Title>
-            <SubTitle>
-              I design and build user-friendly digital
-              products by following industry best practices.
-            </SubTitle>
-            <CTAWrapper>
-              <Button>Got a project</Button>
-              <SecondaryButton>Let's talk</SecondaryButton>
-            </CTAWrapper>
-          </Content>
+          <Hero />
         </Container>
         <Container>
           <AnimatedParagraph sentence="love going out on the weekend with friends to have fun and chillax for the new work week" />
@@ -32,74 +19,9 @@ function App() {
   );
 }
 
-export default App;
-
-const CTAWrapper = styled.div`
-  margin-top: 24px;
-  display: flex;
-  gap: 16px;
-`;
-
 const Container = styled.div`
   max-width: 1280px;
   padding: 24px 48px;
 `;
 
-const Content = styled.div`
-  max-width: 720px;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-`;
-
-const Intro = styled.span`
-  font-size: 24px;
-  font-weight: 600;
-  letter-spacing: normal;
-  line-height: 1.43;
-`;
-
-const SubTitle = styled.p`
-  margin-top: 16px;
-  font-size: 22px;
-  font-weight: 500;
-  line-height: 1.43;
-  color: #403d3d;
-`;
-
-const Title = styled.h1`
-  font-size: 70px;
-  line-height: 70px;
-  font-weight: 700;
-  max-width: 720px;
-  letter-spacing: -0.04em;
-`;
-
-const Button = styled.button`
-  font-size: 16px;
-  font-weight: 500;
-  height: 44px;
-  padding: 12px 24px;
-  cursor: pointer;
-  background-color: #2b197c;
-  border: 2px solid #2b197c;
-  color: #fff;
-  border-radius: 8px;
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const SecondaryButton = styled(Button)`
-  color: #2b197c;
-  background-color: #fff;
-`;
-
-const MotionSentence = styled.div`
-  font-weight: 600;
-  font-size: 32px;
-  p {
-    opacity: 0.87;
-    display: inline-block;
-  }
-`;
+export default App;
---
+++ src/components/Hero.tsx
@@ -0,0 +1,86 @@
+import styled from "@emotion/styled";
+
+export const Hero = () => (
+  <section>
+    <Content>
+      <Intro>
+        Hello ✋,
+        <br />
+      </Intro>
+      <Title>am Peter a Front-End Developer</Title>
+      <SubTitle>
+        I design and build user-friendly digital products by
+        following industry best practices.
+      </SubTitle>
+      <CTAWrapper>
+        <Button>Got a project</Button>
+        <SecondaryButton>Let's talk</SecondaryButton>
+      </CTAWrapper>
+    </Content>
+  </section>
+);
+
+const CTAWrapper = styled.div`
+  margin-top: 24px;
+  display: flex;
+  gap: 16px;
+`;
+
+const Content = styled.div`
+  max-width: 720px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+`;
+
+const Intro = styled.span`
+  font-size: 24px;
+  font-weight: 600;
+  letter-spacing: normal;
+  line-height: 1.43;
+`;
+
+const SubTitle = styled.p`
+  margin-top: 16px;
+  font-size: 22px;
+  font-weight: 500;
+  line-height: 1.43;
+  color: #403d3d;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+  line-height: 70px;
+  font-weight: 700;
+  max-width: 720px;
+  letter-spacing: -0.04em;
+`;
+
+const Button = styled.button`
+  font-size: 16px;
+  font-weight: 500;
+  height: 44px;
+  padding: 12px 24px;
+  cursor: pointer;
+  background-color: #2b197c;
+  border: 2px solid #2b197c;
+  color: #fff;
+  border-radius: 8px;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const SecondaryButton = styled(Button)`
+  color: #2b197c;
+  background-color: #fff;
+`;
+
+const MotionSentence = styled.div`
+  font-weight: 600;
+  font-size: 32px;
+  p {
+    opacity: 0.87;
+    display: inline-block;
+  }
+`;
