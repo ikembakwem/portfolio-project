181537d82271b6ad375afb7657d94ba9a41c4b63
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2023-07-08 13:49:29 +0100

    Add content to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,27 @@
 import styled from "@emotion/styled";
+import { Container } from "./Layouts";
 
-export const Footer = () => <footer>Footer here</footer>;
+export const Footer = () => (
+  <FooterStyles>
+    <Container>
+      <Content>
+        <div>Ikechukwu Mbakwem</div>
+        <div>Copyright ©2023</div>
+      </Content>
+    </Container>
+  </FooterStyles>
+);
 
 const FooterStyles = styled.footer`
+  display: flex;
+
   background-color: yellowgreen;
   border: 4px solid blueviolet;
+`;
+
+const Content = styled.div`
+  display: flex;
+  div {
+    flex: 1;
+  }
 `;
--- src/components/Layouts.tsx
+++ src/components/Layouts.tsx
@@ -4,4 +4,5 @@ export const Container = styled.div`
   max-width: 1280px;
   padding: 24px 48px;
   margin: 0 auto;
+  width: 100%;
 `;
